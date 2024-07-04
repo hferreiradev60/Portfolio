7fa089dc6e199645479d85773e47c1f19cea557c
HugoFrr <ferreira.hugo602@gmail.com>
2024-07-04 16:18:51 +0200

    Fix confilcts

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -1,5 +1,6 @@
 import { Section } from "./Section";
 import { Code } from "./Code";
+import Image from 'next/image';
 
 export default function Hero() {
   return (
@@ -7,10 +8,10 @@ export default function Hero() {
         <div className="flex-[2] flex flex-col gap-2">
             <h2 className="font-caption font-bold text-5xl text-primary">Hugo Ferreira</h2>
             <h3 className="font-caption text-3xl">Student at the University of Amiens</h3>
-            <p className="text-base">1st MIAGE master's student in Amiens (France). I want to be a <Code>Frontend developer</Code>.</p>
+            <p className="text-base">1st MIAGE master&apos;s student in Amiens (France). I want to be a <Code>Frontend developer</Code>.</p> {/* Remplacement de ' par &apos; */}
         </div>
         <div className="flex-1">
-            <img src="profile.png" className="w-full h-auto max-w-xs rounded-full" alt="Hugo's picture" />
+            <Image src="/profile.png" className="w-full h-auto max-w-xs rounded-full" alt="Hugo's picture" width={500} height={500} /> {/* Remplacement de <img> par <Image> */}
         </div>
     </Section>
   )
