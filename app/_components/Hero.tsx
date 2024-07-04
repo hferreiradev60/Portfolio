3a47fa774dfb9c8de536d7cbabf31582913067e6
HugoFrr <ferreira.hugo602@gmail.com>
2024-07-04 16:32:18 +0200

    Fix image paths for next/image

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -1,18 +1,18 @@
 import { Section } from "./Section";
 import { Code } from "./Code";
-import Image from 'next/image';
+import Image from "next/image";
 
 export default function Hero() {
   return (
     <Section className="flex max-lg:flex-col items-start gap-4">
-        <div className="flex-[2] flex flex-col gap-2">
-            <h2 className="font-caption font-bold text-5xl text-primary">Hugo Ferreira</h2>
-            <h3 className="font-caption text-3xl">Student at the University of Amiens</h3>
-            <p className="text-base">1st MIAGE master&apos;s student in Amiens (France). I want to be a <Code>Frontend developer</Code>.</p> {/* Remplacement de ' par &apos; */}
-        </div>
-        <div className="flex-1">
-            <Image src="/profile.png" className="w-full h-auto max-w-xs rounded-full" alt="Hugo's picture" width={500} height={500} /> {/* Remplacement de <img> par <Image> */}
-        </div>
+      <div className="flex-[2] flex flex-col gap-2">
+        <h2 className="font-caption font-bold text-5xl text-primary">Hugo Ferreira</h2>
+        <h3 className="font-caption text-3xl">Student at the University of Amiens</h3>
+        <p className="text-base">1st MIAGE master's student in Amiens (France). I want to be a <Code>Frontend developer</Code>.</p>
+      </div>
+      <div className="flex-1">
+        <Image src="/profile.png" width={200} height={200} className="w-full h-auto max-w-xs rounded-full" alt="Hugo's picture" />
+      </div>
     </Section>
-  )
+  );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
-      return [
-        {
-          source: '/projects/:id',
-          destination: '/projects/project?id=:id'
-        }
-      ];
-    }
-  };
-  
-  export default nextConfig;
+  async rewrites() {
+    return [
+      {
+        source: '/projects/:id',
+        destination: '/projects/project?id=:id'
+      }
+    ];
+  },
+  images: {
+    domains: ['media.licdn.com', 'pbs.twimg.com'],
+  },
+};
+
+export default nextConfig;
